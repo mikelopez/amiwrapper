ce9b9185f57899c67d33a1ca96d1972fc2615081
Mike Lopez <dev@scidentify.info>
2013-07-28 04:38:55 -0400

    initial import

---
+++ README.rst
@@ -0,0 +1,4 @@
+Readme for amiwrapper
+------------------------------
+
+``from amiwrapper import *``
---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(name='amiwrapper',
+      version='0.1',
+      description='Description for amiwrapper',
+      author='',
+      license='MIT',
+      py_modules=['amiwrapper'],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Terminals'
+          ]
+      )
