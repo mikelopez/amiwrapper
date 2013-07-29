ecc05c6acf2635122de6ed891d4c540fddb8e0d7
Mike Lopez <dev@scidentify.info>
2013-07-29 01:55:14 -0400

    moved vars

--- amiwrapper/amiwrapper.py
+++ amiwrapper/amiwrapper.py
@@ -2,16 +2,21 @@ from termprint import *
 from starpy import manager
 from twisted.internet import reactor
 import sys, logging
-import settings
+try:
+    import settings
+    user = getattr(settings, "AMI_USER")
+    pwd = getattr(settings, "AMI_PASS")
+    host = getattr(settings, "PBX")
+except ImportError:
+    host, user, pwd = "", "", ""
 
 i, e, w = "INFO", "ERROR", "WARNING"
 
 class AMIWrapper(object):
     """ Base class wrapper file for call originating with starpy """
-
-    user = getattr(settings, "AMI_USER")
-    pwd = getattr(settings, "AMI_PASS")
-    host = getattr(settings, "PBX")
+    host = host
+    user = user
+    pwd = pwd
     command_txt = None
     response = None
 
@@ -27,8 +32,7 @@ class AMIWrapper(object):
             if k in self.allowed_keys:
                 setattr(self, k, v)
     
-    # privates
-    def __stop_reactor(self):
+    def stop_reactor(self):
         """ Attempt to stop the reactor so the
         application can terminate.
         """
@@ -38,33 +42,24 @@ class AMIWrapper(object):
             termprint(e, "Failed to stop reactor")
             pass
 
-    def __run_reactor(self, method):
+    def run_reactor(self, method):
         """ Start and run the reactor """
         reactor.callWhenRunning(method)
         reactor.run()
 
-    def __exception(self, msg, exit=True):
+    def exception(self, msg, exit=True):
         """ Print any errors and exit """
         termprint(e, msg)
-        self.__stop_reactor()
+        self.stop_reactor()
         sys.exit()
 
-    def __set_session(self):
+    def set_session(self):
         self.session = manager.AMIFactory(self.user, self.pwd)
-        return self.__get_session()
+        return self.get_session()
 
-    def __get_session(self):
+    def get_session(self):
         """ Get the session from self.session safely """
         return getattr(self, "session", None)
 
-
-
-if __name__ == '__main__':
-    #manager.log.setLevel(logging.DEBUG)
-
-    # send a command
-    cl = AMIWrapper(command="dialplan show from-internal")
-    reactor.callWhenRunning(cl.command)
-    reactor.run()
 
 
